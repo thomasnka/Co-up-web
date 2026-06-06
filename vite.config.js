8803a27986c6964360f4bd9760000ec9a0e1fdc2
thomasnka <tuannka@gmail.com>
2026-06-06 18:54:24 +0700

    fix: remove cloudflare vite plugin (breaks Vercel build)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
-  plugins: [react(), cloudflare()],
-  server: { port: 3000 }
+    plugins: [react()],
+    server: { port: 3000 }
 })
